9a7709f486078e46961e45d648bf99d54dbf8feb
Hans Freitag <zem.nospam.20@fnordpol.de>
2015-06-01 22:56:52 +0200

    debugged qsp foo

--- local_cmd/qsp.pl
+++ local_cmd/qsp.pl
@@ -7,30 +7,28 @@
 my ($self, $line) = @_;
 my @out;
 
-push @out, "qsp $line\n";
+my ($call, @refnums)=split(/\s+/, $line);
 
-#my ($call, @refnums)=split(/\s+/, $line);
-#
-#if ( $call =~ /^..$/ ) { 
-#	# I guess there is no call and call is a refnum
-#	push @refnum, $call; 
-#	$call=$self->call;
-#}
+if ( $call =~ /^..$/ ) { 
+	# I guess there is no call and call is a refnum
+	push @refnums, $call; 
+	$call=$self->call;
+}
+$call=$self->{qtc_query}->allowed_letters_for_call(lc($call));
 
-#print STDERR "qsp $call @refnum\n";
 
 ## send qsp 
-#foreach my $ref (@refnums) {
+foreach my $ref (@refnums) {
 #	print STDERR "Telegram by ref\n";
-#	my $telegram=$self->{qtc_query}->telegram_by_refnum($ref, lc($call)); 
-#	if ( ! $telegram ) { next; }
-#	print STDERR "publish $telegram\n";
-#	# we have a telegram, let us qsp.... 
-#	$self->{qtc_publish}->qsp(
-#		to=>lc($to_call),
-#		msg=>$telegram, 
-#		set_of_comment=>"dxspider call ".$self->{qtc_query}->allowed_letters_for_telegram(lc($call)),
-#	); 
-#}
+	my $telegram=$self->{qtc_query}->telegram_by_refnum($ref, $call); 
+	if ( ! $telegram ) { push @out, "$ref not found for $call"; next; }
+	push @out, "qsp $ref to $call";
+	# we have a telegram, let us qsp.... 
+	$self->{qtc_publish}->qsp(
+		to=>$call,
+		msg=>$telegram, 
+		set_of_comment=>"dxspider call ".$self->{qtc_query}->allowed_letters_for_telegram(lc($self->call)),
+	); 
+}
 
 return (1, @out);
