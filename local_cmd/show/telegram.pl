251be8c1a6015792d83ce000c795bc61cf127073
Hans Freitag <zem.nospam.20@fnordpol.de>
2015-06-01 20:05:19 +0200

    sh/telegram for yourself

--- local_cmd/show/telegram.pl
+++ local_cmd/show/telegram.pl
@@ -10,7 +10,14 @@ my @out;
 
 my ($call, $type, $anz, $offset)=split(/\s+/, $line);
 
-if ( ! $call ) { $call=$self->call() }
+if ( $call =~ /^(new)|(timeline_new)|(timeline)|(all)|(sent)$/ ) { 
+	# $call is a type
+	$offset=$anz; 
+	$anz=$type; 
+	$type=$call; 
+	$call=$self->call();
+}
+if ( ! $call ) { $call=$self->call(); }
 
 foreach my $msg ($self->{qtc_query}->list_telegrams(lc($call), $type, int($anz), int($offset))) {
 	#push @out, $msg->filename; 
