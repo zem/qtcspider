f54a105a0a5cba311f9a0e1d5d2d791a084a4c1f
Hans Freitag <zem.nospam.20@fnordpol.de>
2015-06-01 19:09:45 +0200

    would send a qsp if we would have a working publisher

--- local_cmd/qsp.pl
+++ local_cmd/qsp.pl
@@ -9,6 +9,19 @@
 my ($self, $line) = @_;
 my @out;
 
+my ($call, @refnums)=split(/\s+/, $line);
+
 # send qsp 
+foreach my $ref (@refnums) {
+	my $telegram=$self->{qtc_query}->telegram_by_refnum($ref, lc($call)); 
+	if ( ! $telegram ) { next; }
+	# we have a telegram, let us qsp.... 
+	
+	#$publish->qsp(
+	#	to=>lc($to_call),
+	#	msg=>$telegram, 
+	#	set_of_comments=>"dxspider call ".$self->{qtc_query}->allowed_letters_for_telegram(lc($call)),
+	#); 
+}
 
 return (1, @out);
